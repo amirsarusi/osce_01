87234d79407be9cc15b1a062b7a3efed0df06fac
amir <amir0sarusi@gmail.com>
2019-11-26 10:59:27 +0200

    makefile

---
+++ cmake-build-debug/CMakeFiles/osce_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/osce_01.dir/main.c.o"
+  "osce_01"
+  "osce_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/osce_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
